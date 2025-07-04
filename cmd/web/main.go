da0b28ad33c4379ca79351875b3a27864fe11c62
kevinfjq <kevifjq@gmail.com>
2025-07-03 23:24:40 -0300

    feat: add base HTML template and implement home handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
@@ -16,4 +21,33 @@ func main() {
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
